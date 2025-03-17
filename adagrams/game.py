fa19958ee4e51fdc33d5121d9f86d93759c7ac5e
Renata <murzinarenata@gmail.com>
2025-03-16 21:53:52 -0700

    constans updated

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,15 +1,27 @@
 from random import randint
 
-def draw_letters():
-    hand = []
-    letters_dictionary ={
+LETTERS_DICT ={
     'A': 9, 'B': 2, 'C': 2, 'D': 4, 'E': 12, 'F': 2, 'G': 3, 'H': 2, 'I': 9, 'J': 1, 
     'K': 1, 'L': 4, 'M': 2, 'N': 6, 'O': 8, 'P': 2, 'Q': 1, 'R': 6, 'S': 4, 'T': 6, 
     'U': 4, 'V': 2, 'W': 2, 'X': 1, 'Y': 2, 'Z': 1
     }
 
+LETTERS_SCORE = {
+    'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+    'D': 2, 'G': 2,
+    'B': 3, 'C': 3, 'M': 3, 'P': 3,
+    'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+    'K': 5,
+    'J': 8, 'X': 8,
+    'Q': 10, 'Z': 10
+}
+
+
+def draw_letters():
+    hand = []
+
     letters_list = []
-    for letter, count in letters_dictionary.items():
+    for letter, count in LETTERS_DICT.items():
         letters_list.extend(letter * count)
     
     for i in range(10):
@@ -19,9 +31,11 @@ def draw_letters():
         letters_list.pop(random_letter_index)
 
     return hand
+
 def uses_available_letters(word, letter_bank):
     word_upper = word.upper()
     bank_dict = {}
+
     for letter in letter_bank:
         if letter in bank_dict:
             bank_dict[letter] += 1
@@ -36,20 +50,11 @@ def uses_available_letters(word, letter_bank):
     return True
 
 def score_word(word):
-    letter_scores = {
-    'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
-    'D': 2, 'G': 2,
-    'B': 3, 'C': 3, 'M': 3, 'P': 3,
-    'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
-    'K': 5,
-    'J': 8, 'X': 8,
-    'Q': 10, 'Z': 10
-}
     word_upper = word.upper()
     score = 0
 
     for letter in word_upper:
-        score += letter_scores[letter]
+        score += LETTERS_SCORE[letter]
 
     if len(word) > 6:
         score += 8
